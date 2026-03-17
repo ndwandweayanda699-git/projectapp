df13fdfdaf5f5a51bd705ee8231dbeaea31fe63b
ndwandweayanda699-git <ayanda>
2026-03-16 20:49:14 -0700

    added success route

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app' // This connects to your app.tsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import App from './app';
+import Success from './success';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+    <BrowserRouter>
+
+      <Routes>
+
+        <Route path="/" element={<App />} />
+        <Route path="/success" element={<Success />} />
+
+      </Routes>
+
+    </BrowserRouter>
+
+  </React.StrictMode>
+);
